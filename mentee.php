ef9d6efaa45b34466cd515a3a8676d97d417af98
Francis Priestland <francis.priestland@conduce.net>
2014-11-15 10:26:15 +0000

    small

--- mentee.php
+++ mentee.php
@@ -144,6 +144,7 @@
                 echo "<h2 class=\"blog-post-title\">".$bro['title']."</h2>";
                 echo "<p class=\"blog-post-meta\">".$bro['date']." by ".$row['menteeName']."</p>";
                 echo "<p>".$bro['description']."</p>";
+                echo "<p>".$bro['photo1']."</p>";
                 if($bro.['photo1']!== NULL){
                   var_dump($bro.['photo1']);
                   if($bro.['photo2']!== NULL){
