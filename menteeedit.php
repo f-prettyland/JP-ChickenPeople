2f07d10780528e85aeaf6ee36bf938c148d001a7
Paula <pjc8@st-andrews.ac.uk>
2014-11-15 06:05:07 +0000

    test

--- menteeedit.php
+++ menteeedit.php
@@ -85,8 +85,8 @@
 <div class="row">
         <div class="col-sm-6">
           <div class="panel panel-default">
-            <div class="panel-heading" style="background-color:#F5DA81">
-              <h3 class="panel-title">Your details</h3>
+            <div class="panel-heading">
+              <h3 class="panel-title" style="background-color:#F5DA81">Your details</h3>
             </div>
             <div class="panel-body">
              City:<br>
@@ -120,13 +120,13 @@
       </div>
       <div class="col-sm-6">
           <div class="panel panel-default">
-            <div class="panel-heading" style="background-color:#F5DA81">
-              <h3 class="panel-title">Your story</h3>
+            <div class="panel-heading" >
+              <h3 class="panel-title" style="background-color:#F5DA81">Your story</h3>
             </div>
             <div class="panel-body">
                             
               Story<br>
-              <textarea rows="10" cols="55" name="story">
+              <textarea rows="10" cols="50" name="story">
               <?php echo "\"".$row['story']."\""; ?></textarea>
                <button type="button" class="btn btn-sm btn-warning">Submit</button> 
             </div>
