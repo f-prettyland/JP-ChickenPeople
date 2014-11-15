e5ad3eb0096ae82a49423db36fb0164a5b593486
victoreo <vicand93@gmail.com>
2014-11-15 08:15:03 +0000

    aefjkjelk;f

--- mentee.php
+++ mentee.php
@@ -86,7 +86,7 @@
   <ul class="nav nav-tabs" role="tablist">
     <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
     <li role="presentation"><a href="#timeline" aria-controls="timeline" role="tab" data-toggle="tab">Timeline</a></li>
-    <li role="presentation"><a href="#messages" aria-controls="contact" role="tab" data-toggle="tab">Contact</a></li>
+    <li role="presentation"><a href="#contact" aria-controls="contact" role="tab" data-toggle="tab">Contact</a></li>
   </ul>
 
   <!-- Tab panes -->
