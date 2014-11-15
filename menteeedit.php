ee96a5973148bfe943578253ab5a7304042e3e68
Paula <pjc8@st-andrews.ac.uk>
2014-11-15 05:57:23 +0000

    test

--- menteeedit.php
+++ menteeedit.php
@@ -92,25 +92,9 @@
               Panel content
             </div>
           </div>
-           <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title">Panel title</h3>
-            </div>
-            <div class="panel-body">
-              Panel content
-            </div>
-          </div>
       </div>
       <div class="col-sm-4">
           <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title">Panel title</h3>
-            </div>
-            <div class="panel-body">
-              Panel content
-            </div>
-          </div>
-           <div class="panel panel-default">
             <div class="panel-heading">
               <h3 class="panel-title">Panel title</h3>
             </div>
