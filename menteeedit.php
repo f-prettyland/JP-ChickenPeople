eb42c754b09c35e5d33b2eda7ec3e5086849264f
Paula <pjc8@st-andrews.ac.uk>
2014-11-15 06:09:53 +0000

    test

--- menteeedit.php
+++ menteeedit.php
@@ -91,30 +91,30 @@
             <div class="panel-body">
              City:<br>
               <input type="text" name="city" value =<?php echo "\"".$row['city']."\""; ?> size="25">
-               <button type="button" class="btn btn-sm btn-warning">Submit</button> 
+               <button type="button" class="btn btn-sm btn-warning">Modify</button> 
               <br>
               Country:<br>
               <input type="text" name="country" value=<?php echo "\"".$row['country']."\""; ?> size="25">
-               <button type="button" class="btn btn-sm btn-warning">Submit</button> 
+               <button type="button" class="btn btn-sm btn-warning">Modify</button> 
               <br>
               Product:<br>
               <input type="text" name="product" value=<?php echo "\"".$row['product']."\""; ?> size="25">
-               <button type="button" class="btn btn-sm btn-warning">Submit</button> 
+               <button type="button" class="btn btn-sm btn-warning">Modify</button> 
               <br>
               Tag:<br>
               <input type="text" name="tag" value=<?php echo "\"".$row['tag']."\""; ?> size="25">
-               <button type="button" class="btn btn-sm btn-warning">Submit</button> 
+               <button type="button" class="btn btn-sm btn-warning">Modify</button> 
               <br>
               Gmail:<br>
               <input type="text" name="gmail" value=<?php echo "\"".$row['gmail']."\""; ?> size="25">
-               <button type="button" class="btn btn-sm btn-warning">Submit</button> 
+               <button type="button" class="btn btn-sm btn-warning">Modify</button> 
               <br>
               Phone Number:<br>
               <input type="text" name="phone" value=<?php echo "\"".$row['phone']."\""; ?> size="25">
-               <button type="button" class="btn btn-sm btn-warning">Submit</button> 
+               <button type="button" class="btn btn-sm btn-warning">Modify</button> 
               <br>
               Do you want your contact details to remain private?<br>
-              <input type="checkbox" name="option3" value="Cheese"> Protext my data<br> 
+              <input type="checkbox" name="option3" value="Cheese">Protect my data<br> 
             </div>
           </div>
       </div>
@@ -124,11 +124,9 @@
               <h3 class="panel-title" >Your story</h3>
             </div>
             <div class="panel-body">
-                            
-              Story<br>
-              <textarea rows="10" cols="50" name="story">
+              <textarea rows="10" cols="60" name="story">
               <?php echo "\"".$row['story']."\""; ?></textarea>
-               <button type="button" class="btn btn-sm btn-warning">Submit</button> 
+               <button type="button" class="btn btn-sm btn-warning">Modify</button> 
             </div>
           </div>
       </div>
