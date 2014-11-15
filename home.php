afc226be90e9ec7806814a23ba0a4767557ea5f8
ievavei <vasiljeva.ieva@gmail.com>
2014-11-15 09:01:01 +0000

    fhj

--- home.php
+++ home.php
@@ -66,7 +66,7 @@ map.mapTypes.set(layer, new google.maps.StamenMapType(layer));
       or die("Unable to connect to MySQL");
     mysql_select_db("data") or die(mysql_error());
 
-  $result = mysql_query("SELECT menteeId, menteeName, city, country, product, tag from mentees")
+  $result = mysql_query("SELECT menteeId, menteeName, city, country, picName from mentees")
       or die(mysql_error()); 
 
   while(($row = mysql_fetch_assoc($result)) != NULL) {
